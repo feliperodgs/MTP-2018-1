5ff07dd0c27095b83fadb96e83cece1d361d17c4
feliperodgs <38439497+feliperodgs@users.noreply.github.com>
2018-06-25 17:22:48 -0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -3,191 +3,164 @@
 
 #include <stdio.h>
 
-void dec_all(int base)
-{	int resto[100], num, i, j=0, guarda;
-	printf("\nDigite o valor em decimal a ser convertido: ");
-	scanf("%d", &num);
-	guarda = num;
-	for(i=0; num >= base; i++)
-	{	resto[i] = num%base;
-		num = num/base;
-		if(num < base)
-		{	i++;
-			resto[i] = num;		}
-	} i--;
-	if(base==16)
-	{	printf("\nValor convertido: ");
-		if(guarda >= base)
-		{	do{	printf("%X", resto[i]);
-			i--;	}while(i>=0); 	}
-		else if(guarda < base)
-		{	printf("%X", num);	}
-	}
-	else
-	{	printf("\nValor convertido: ");
-		do{	printf("%d", resto[i]);
-			i--;	}while(i>=0); 	}	
-	printf("\n\n");
-}
-void bin_all(int base)
-{	int  resto1[100], resto2[100], num, i, guarda, j=0, n=1, soma=0, k=0;
-	char resto[100];
-
-	printf("\nDigite o valor em binario a ser convertido: ");
-	scanf("%s", &resto);
-	for(i=0; resto[i] != '\0'; i++);
-	i--;
-	k=i;
-	do
-	{	if(resto[i] == '0') {resto1[i] = 0; }
-		else if(resto[i] == '1') {resto1[i] = 1;}
-		i--;
-	}while(i>=0);
-	i = k;
-	do
-	{	for(k=0; k<j; k++)
-		{	n*=2;	}
-		soma += resto1[i]*n;
-		n=1;
-		i--;
-		j++;
-	}while(i>=0);
-	if(base != 16)
-	{	printf("\nValor convertido: %d", soma);	}
-	
-	if(base == 16)
-	{	if(soma >= base)
-		{for(i=0; soma >= base; i++)
-		{	resto2[i] = soma%base;
-			soma = soma/base;
-			if(soma < base)
-			{	i++;
-				resto2[i] = soma;		}
-		} i--;
-		if(base == 16)
-		{	printf("\nValor convertido: ");
-			do{	printf("%X", resto2[i]);
-				i--;	}while(i>=0); 	}	}
-		else
-		{	resto2[i] = soma;
-			if(base == 16)
-			{	printf("\nValor convertido: ");
-				do{	printf("%X", resto2[i]);
-					i--;	}while(i>=0); 	}	}
-	}	
-	printf("\n\n");
-}
-
-void hex_all(int base)
-{	char resto[100];
-	int resto1[100], num, i, guarda, j=0, n=1, soma=0, k=0;
-	printf("\nDigite o valor em hexadecimal a ser convertido: ");
-	scanf("%s", &resto);
-	for(i=0; resto[i] != '\0'; i++);
-	i--;
-	guarda = i;
-	k=i;
-	do
-	{	if(resto[i] == 'A' || resto[i] == 'a') {resto[i] = 10; }
-		else if(resto[i] == 'B' || resto[i] == 'b') {resto[i] = 11; }
-		else if(resto[i] == 'C' || resto[i] == 'c') {resto[i] = 12; }
-		else if(resto[i] == 'D' || resto[i] == 'd') {resto[i] = 13; }
-		else if(resto[i] == 'E' || resto[i] == 'e') {resto[i] = 14; }
-		else if(resto[i] == 'F' || resto[i] == 'f') {resto[i] = 15; }
-		else {	resto[i] -= 48;		}	i--;							}while(i>=0);
-	i = guarda;
-	do
-	{	for(k=0; k<j; k++)
-		{	n*=16;	}
-		soma += resto[i]*n;
-		n=1;	i--;	j++;
-	}while(i>=0);
-	if(base != 2)
-	{	printf("\nValor convertido: %d", soma);	}
-	if(base == 2)
-	{	for(i=0; soma >= base; i++)
-		{	resto[i] = soma%base;
-			soma = soma/base;
-			if(soma < base)
-			{	i++;
-				resto[i] = soma;		}
-		} i--;
-		printf("\nValor convertido: ");
-		do{	printf("%d", resto[i]);
-			i--;	}while(i>=0); 	}
-	printf("\n\n");
-}
-
-void oct_dec(int base)
-{	char resto[100];
-	int resto1[100], num, i, guarda, j=0, n=1, soma=0, k=0;
-	printf("\nDigite o valor em octal a ser convertido: ");
-	scanf("%s", &resto);
-	for(i=0; resto[i] != '\0'; i++);
-	i--;
-	guarda = i;
-	k=i;
-	do
-	{	resto[i] -= 48;
-		i--;			}while(i>=0);
-	i = guarda;
-	do
-	{	for(k=0; k<j; k++)
-		{	n*=8;	}
-		soma += resto[i]*n;
-		n=1;	i--;	j++;
-	}while(i>=0);
-	printf("\nValor convertido: %d", soma);	printf("\n\n");	}
-
-int main ()
+void preenche(char bits[])
 {
-	int opcao, hex=16, bin=2, oct=8;
-
-	do
-	{	printf("###   MENU DE CONVERSOES DE BASES NUMERICAS   ###");
-		printf("\n###         1-Binario para Decimal            ###");
-		printf("\n###         2-Binario para Hexadecimal        ###");
-		printf("\n###         3-Hexadecimal para Decimal        ###");
-		printf("\n###         4-Hexadecimal para Binario        ###");
-		printf("\n###         5-Decimal para Binario            ###");
-		printf("\n###         6-Decimal para Hexadecimal        ###");
-		printf("\n###         7-Octal para Decimal              ###");
-		printf("\n###         8-Decimal para Octal              ###");
-		printf("\n###         9-Sair do programa                ###");
-		printf("\nSua opcao e: ");
-		scanf("%d", &opcao);
-		
-		switch(opcao)
-		{	case 1:
-				bin_all(bin);
-				break;
-			case 2:
-				bin_all(hex);
-				break;
-			case 3:
-				hex_all(hex);
-				break;
-			case 4:
-				hex_all(bin);
-				break;
-			case 5:
-				dec_all(bin);
-				break;
-			case 6:
-				dec_all(hex);
-				break;
-			case 7:
-				oct_dec(oct);
-				break;
-			case 8:
-				dec_all(oct);
-				break;
-			case 9:
-				break;
-			default:
-				break;
+	printf("\nPreencha com o valor a ser convertido: ");
+	scanf("%s", bits);
+}
+int conv2oct(int num, int b)
+{
+    int aux=0, i=1;
+    while (num!=0)
+    {
+        aux+=(num%b)*i;
+        num/=b;
+        i*=10;
+    }
+    return aux;
+}
+void conv2hex(int num, int b, int menu)
+{
+	int i=0, j=0, k=0, aux=0, n=0;
+	char hex[256], temp[256];
+	k=num;
+	while (k!=0)
+	{
+		aux=k%b;
+		if (aux<10)
+			temp[j++]=48+aux;
+		else
+		    temp[j++]=55+aux;
+		k=k/b;
+		n++;
+	}
+	for (i=n-1, j=0; i+1!=0; --i, ++j)
+		hex[j]=temp[i];
+	hex[n]='\0';
+	printf("\n| \"%i\" + \"%i\" | \"%s\"|\n", menu, num, hex);
+}
+int conv2dec(char bits[], int b) 
+{
+	int i=0, aux=0;
+	for(i=0; bits[i]!='\0'; i++)
+	{
+	    if (bits[i]<=57 && bits[i]>=48)
+	    {
+            aux=aux*b+(bits[i] - '0');
+	    }
+        else if (bits[i]>=65)
+        {
+            {
+        	    if (bits[i]>=97)
+    	        {
+    	        	bits[i]-=32;
+    	        	aux=aux*b+(bits[i] - '7');
+				}
+    	        else
+        	        aux=aux*b+(bits[i] - '7');
+		    }
+	    }
+    }
+    return aux;
+}
+int conv2bin(int num, int b)
+{
+	char bin[256];
+	int i=0, j=0, k=1, n=0, aux=0, aux2;
+	for (n=0; num>=(1<<n); n++);
+	for (i=1; i<=n; i++)
+	{
+		if (num%2==0)
+		{
+			bin[n-i]='0';
+			num=num/2;
 		}
-	}while(opcao != 9);
-	
+		else
+		{
+			bin[n-i]='1';
+			num=(num-1)/2;
+		}
+	}
+	for (i=0; bin[i]!='\0'; i++)
+	{
+		k=1;
+		for(j=1; j<(n-i); j++)
+		    k=k*b;
+		aux+=(bin[i] - '0')*(k);
+	}
+    return aux;
+}
+int main()
+{
+	char bits[256];
+	int menu=0, b=0, cv=0, num=0, test=0;
+	do
+	{
+		printf("\nEscolha a conversao: \n[1]Binario para Decimal \n[2]Binario para Hexadecimal \n[3]Hexadecimal para Decimal \n[4]Hexadecimal para Binario \n[5]Decimal para Binario \n[6]Decimal para Hexadecimal \n[7]Octal para Decimal \n[8]Decimal para Octal \n[9]Sair do programa \n:");
+		scanf("%i", &menu);
+		switch (menu)
+		{
+			case 1: // Bin -> Dec
+				b=2;
+				preenche(bits);
+                cv=conv2dec(bits, b);
+				printf("\n| \"%i\" + \"%s\" | \"%i\" |\n", menu, bits, cv);
+				break;
+			case 2: // Bin -> Hex
+			    b=2;
+			    preenche(bits);
+			    num=conv2dec(bits, b);
+			    b=16;
+                conv2hex(num, b, menu);
+				break;
+			case 3: // Hex -> Dec
+				b=16;
+				preenche(bits);
+                cv=conv2dec(bits, b);
+				printf("\n| \"%i\" + \"%s\" | \"%i\" |\n", menu, bits, cv);
+				break;
+			case 4: // Hex -> Bin
+				b=16;
+				preenche(bits);
+				num=conv2dec(bits, b);
+				b=10;
+				cv=conv2bin(num, b);
+				printf("\n| \"%i\" + \"%s\" | \"%i\" |\n", menu, bits, cv);
+				num=0;
+				break;
+			case 5: // Dec -> Bin
+				b=10;
+				printf("\nPreencha com o valor a ser convertido: ");
+	            scanf("%i", &num);
+                cv=conv2bin(num, b);
+				printf("\n| \"%i\" + \"%i\" | \"%i\" |\n", menu, num, cv);
+				break;
+			case 6: // Dec -> Hex
+			    b=16;
+			    printf("\nPreencha com o valor a ser convertido: ");
+	            scanf("%i", &num);
+                conv2hex(num, b, menu);
+				break;
+			case 7: // Oc -> Dec
+				b=8;
+				preenche(bits);
+                cv=conv2dec(bits, b);
+				printf("\n| \"%i\" + \"%s\" | \"%i\" |\n", menu, bits, cv);
+				break;
+			case 8: // Dec -> Oc
+			    b=8;
+			    printf("\nPreencha com o valor a ser convertido: ");
+            	scanf("%i", &num);
+			    cv=conv2oct(num, b);
+			    printf("\n| \"%i\" + \"%i\" | \"%i\" |\n", menu, num, cv);
+				break;
+			default: 
+			    if(menu!=9)
+			    {
+			    	printf("\n\nEscolha invalida\n\n");
+			        break;
+				}
+		}
+	}while (menu!=9);
 	return 0;
 }
